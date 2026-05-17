00a3b29f2cb9354c26d79257351d1f317b3ae902
MateoGuillen7 <mateokour77@gmail.com>
2026-05-16 21:07:47 -0500

    Update registro_de_usuarios.sql

--- registro_de_usuarios.sql
+++ registro_de_usuarios.sql
@@ -2,15 +2,7 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 17-05-2026 a las 03:41:26
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Servidor: localhost
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
